8f0db11d2b12a8f2aff6e7e65e6bbca922e784e4
Muhammad Hassaan <hassaan.nauman2008@gmail.com>
2025-02-12 22:09:46 +0500

    after learning object in js this task was given by smit

---
+++ script.js
@@ -0,0 +1,618 @@
+const carShowroom = {
+    cars: {
+      // Lamborghini
+      car1: {
+        model: "Aventador",
+        company: "Lamborghini",
+        image: "./assets/lam aven 1.jpg",
+        price: "$400,000",
+      },
+      car2: {
+        model: "Huracan",
+        company: "Lamborghini",
+        image: "./assets/lam hur 1.jpg",
+        price: "$200,000",
+      },
+      car3: {
+        model: "Urus",
+        company: "Lamborghini",
+        image: "./assets/lam urus 1.jpg",
+        price: "$220,000",
+      },
+      car4: {
+        model: "Sian",
+        company: "Lamborghini",
+        image: "./assets/lam sian 1.jpg",
+        price: "$3,000,000",
+      },
+      car5: {
+        model: "Veneno",
+        company: "Lamborghini",
+        image: "./assets/lam veneno 1.jpg",
+        price: "$4,500,000",
+      },
+      // Honda
+      car6: {
+        model: "Civic",
+        company: "Honda",
+        image: "./assets/honda civic 1.jpg",
+        price: "$25,000",
+      },
+      car7: {
+        model: "Accord",
+        company: "Honda",
+        image: "./assets/honda accord 11.jpg",
+        price: "$30,000",
+      },
+      car8: {
+        model: "CR-V",
+        company: "Honda",
+        image: "./assets/honda cr-v.jpg",
+        price: "$35,000",
+      },
+      car9: {
+        model: "Pilot",
+        company: "Honda",
+        image: "./assets/honda pilot.jpg",
+        price: "$40,000",
+      },
+      car10: {
+        model: "Fit",
+        company: "Honda",
+        image: "./assets/honda fit 1.avif",
+        price: "$18,000",
+      },
+      // Toyota
+      car11: {
+        model: "Corolla",
+        company: "Toyota",
+        image: "./assets/toyota corola.jpg",
+        price: "$22,000",
+      },
+      car12: {
+        model: "Camry",
+        company: "Toyota",
+        image: "./assets/toyota camry.jpg",
+        price: "$28,000",
+      },
+      car13: {
+        model: "Land Cruiser",
+        company: "Toyota",
+        image: "./assets/toyota land cruiser.jpg",
+        price: "$45,000",
+      },
+      car14: {
+        model: "Supra mk5",
+        company: "Toyota",
+        image: "./assets/toyota supra mk5.jpg",
+        price: "$50,000",
+      },
+      car15: {
+        model: "Mark X",
+        company: "Toyota",
+        image: "./assets/toyota mark x.jpg",
+        price: "$35,000",
+      },
+      // Ford
+      car16: {
+        model: "Mustang",
+        company: "Ford",
+        image: "./assets/ford mustang.jpg",
+        price: "$55,000",
+      },
+      car17: {
+        model: "Focus",
+        company: "Ford",
+        image: "./assets/ford focus.jpg",
+        price: "$20,000",
+      },
+      car18: {
+        model: "Explorer",
+        company: "Ford",
+        image: "./assets/ford explorer.jpg",
+        price: "$45,000",
+      },
+      car19: {
+        model: "F-150",
+        company: "Ford",
+        image: "./assets/ford F-150.jpg",
+        price: "$50,000",
+      },
+      car20: {
+        model: "Escape",
+        company: "Ford",
+        image: "./assets/ford escape.jpg",
+        price: "$30,000",
+      },
+      // Audi
+      car21: {
+        model: "A4",
+        company: "Audi",
+        image: "./assets/audi a4.jpg",
+        price: "$40,000",
+      },
+      car22: {
+        model: "Q7",
+        company: "Audi",
+        image: "./assets/audi q7.jpg",
+        price: "$70,000",
+      },
+      car23: {
+        model: "R8",
+        company: "Audi",
+        image: "./assets/audi r8.jpg",
+        price: "$200,000",
+      },
+      car24: {
+        model: "A6",
+        company: "Audi",
+        image: "./assets/audi a6.jpg",
+        price: "$60,000",
+      },
+      car25: {
+        model: "TT",
+        company: "Audi",
+        image: "./assets/audi tt.jpg",
+        price: "$55,000",
+      },
+      // BMW
+      car26: {
+        model: "X5",
+        company: "BMW",
+        image: "./assets/bmw x5.jpg",
+        price: "$65,000",
+      },
+      car27: {
+        model: "M3",
+        company: "BMW",
+        image: "./assets/bmw m3.jpg",
+        price: "$75,000",
+      },
+      car28: {
+        model: "i8",
+        company: "BMW",
+        image: "./assets/bmw i8.jpg",
+        price: "$150,000",
+      },
+      car29: {
+        model: "7 Series",
+        company: "BMW",
+        image: "./assets/bmw 7 series.jpg",
+        price: "$90,000",
+      },
+      car30: {
+        model: "Z4",
+        company: "BMW",
+        image: "./assets/bmw z4.jpg",
+        price: "$65,000",
+      },
+      // Pagani
+      car31: {
+        model: "Huayra",
+        company: "Pagani",
+        image: "./assets/pagani huayra.webp",
+        price: "$3,000,000",
+      },
+      car32: {
+        model: "Zonda",
+        company: "Pagani",
+        image: "./assets/pagani zonda.jpg",
+        price: "$2,500,000",
+      },
+      car33: {
+        model: "Imola",
+        company: "Pagani",
+        image: "./assets/pagani imola.jpg",
+        price: "$5,000,000",
+      },
+      car34: {
+        model: "Utopia",
+        company: "Pagani",
+        image: "./assets/pagani utopia.webp",
+        price: "$4,000,000",
+      },
+      car35: {
+        model: "Codalunga",
+        company: "Pagani",
+        image: "./assets/pagani codalunga.jpg",
+        price: "$7,000,000",
+      },
+      // Bugatti
+      car36: {
+        model: "Veyron",
+        company: "Bugatti",
+        image: "./assets/bugatti veyron.webp",
+        price: "$2,500,000",
+      },
+      car37: {
+        model: "Chiron",
+        company: "Bugatti",
+        image: "./assets/bugatti chiron.jpg",
+        price: "$3,000,000",
+      },
+      car38: {
+        model: "Divo",
+        company: "Bugatti",
+        image: "./assets/bugatti divo.jpg",
+        price: "$5,800,000",
+      },
+      car39: {
+        model: "Centodieci",
+        company: "Bugatti",
+        image: "./assets/bugatti centodieci.jpg",
+        price: "$9,000,000",
+      },
+      car40: {
+        model: "Bolide",
+        company: "Bugatti",
+        image: "./assets/bugatti bolide.jpg",
+        price: "$4,000,000",
+      },
+      // Porsche
+      car41: {
+        model: "911",
+        company: "Porsche",
+        image: "./assets/porsche 911.jpg",
+        price: "$120,000",
+      },
+      car42: {
+        model: "Cayenne",
+        company: "Porsche",
+        image: "./assets/porsche cayenne.webp",
+        price: "$80,000",
+      },
+      car43: {
+        model: "Taycan",
+        company: "Porsche",
+        image: "./assets/parsche taycan.jpg",
+        price: "$100,000",
+      },
+      car44: {
+        model: "Panamera",
+        company: "Porsche",
+        image: "./assets/porsche panamera.jpg",
+        price: "$90,000",
+      },
+      car45: {
+        model: "Macan",
+        company: "Porsche",
+        image: "./assets/porsche macan.jpg",
+        price: "$70,000",
+      },
+      // Bentley
+      car46: {
+        model: "Continental GT",
+        company: "Bentley",
+        image: "./assets/bentley continental gt.jpg",
+        price: "$250,000",
+      },
+      car47: {
+        model: "Bentayga",
+        company: "Bentley",
+        image: "./assets/bentley bentayga.jpg",
+        price: "$200,000",
+      },
+      car48: {
+        model: "Flying Spur",
+        company: "Bentley",
+        image: "./assets/bentley flying spur.jpg",
+        price: "$220,000",
+      },
+      car49: {
+        model: "Mulsanne",
+        company: "Bentley",
+        image: "./assets/bentley mulsanne.jpg",
+        price: "$300,000",
+      },
+      car50: {
+        model: "Azure",
+        company: "Bentley",
+        image: "./assets/bentley azure 2.jpg",
+        price: "$350,000",
+      },
+    },
+  }
+  
+  class Car {
+    constructor(model, company, image, price) {
+      this.model = model
+      this.company = company
+      this.image = image
+      this.price = price
+    }
+  
+    createCard() {
+      return `<div class="car-card">
+                      <img src="${this.image}" alt="${this.model}" width="100%">
+                      <h3>${this.model}</h3>
+                      <p>${this.company}</p>
+                      <p class="price">Price: ${this.price}</p>
+                  </div>`
+    }
+  }
+  
+  class CarSearch {
+    constructor() {
+      this.cars = Object.values(carShowroom.cars).map((car) => new Car(car.model, car.company, car.image, car.price))
+      this.displayCars(this.cars)
+      this.populateCarNames()
+      document.getElementById("search-btn").addEventListener("click", () => this.filterCars())
+      document.getElementById("car-name").addEventListener("change", () => this.updateCarModels())
+    }
+  
+    displayCars(cars) {
+      const container = document.getElementById("car-container")
+      container.innerHTML = cars.map((car) => car.createCard()).join("")
+      container.classList.add("car-grid")
+    }
+  
+    populateCarNames() {
+      const carNameSelect = document.getElementById("car-name")
+      const uniqueCompanies = [...new Set(this.cars.map((car) => car.company))]
+      uniqueCompanies.forEach((company) => {
+        const option = document.createElement("option")
+        option.value = company
+        option.textContent = company
+        carNameSelect.appendChild(option)
+      })
+    }
+  
+    updateCarModels() {
+      const selectedCompany = document.getElementById("car-name").value
+      const carModelSelect = document.getElementById("car-model")
+      carModelSelect.innerHTML = '<option value="">Select Car Model</option>'
+  
+      if (selectedCompany) {
+        const models = this.cars.filter((car) => car.company === selectedCompany).map((car) => car.model)
+        const uniqueModels = [...new Set(models)]
+        uniqueModels.forEach((model) => {
+          const option = document.createElement("option")
+          option.value = model
+          option.textContent = model
+          carModelSelect.appendChild(option)
+        })
+      }
+    }
+  
+    filterCars() {
+      const selectedCompany = document.getElementById("car-name").value
+      const selectedModel = document.getElementById("car-model").value
+  
+      const filteredCars = this.cars.filter(
+        (car) => (!selectedCompany || car.company === selectedCompany) && (!selectedModel || car.model === selectedModel),
+      )
+  
+      this.displayCars(filteredCars)
+    }
+  }
+  
+  new CarSearch()
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
